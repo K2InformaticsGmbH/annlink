818c369be60a93e0ccb8b280b9ab289fbaf2f8de
Agustin Cautin <acautin@gmail.com>
2017-09-21 08:30:30 +0200

    Removing some redundant clauses from the handle_call function in the annlink module and added default values for the network.

--- src/annlink.erl
+++ src/annlink.erl
@@ -11,6 +11,7 @@
     create_neural_network/4,
     supported_activations/0,
     initialize/3,
+    initialize/4,
     add_layer/2,
     add_activation/2,
     set_cost/2,
@@ -40,6 +41,12 @@
     conn :: term()
 }).
 
+%% Default network values.
+-define(LEARNING_RATE, 0.001).
+-define(BATCH_SIZE, 512).
+-define(EPOCHS, 1).
+-define(SCALE, null).
+
 -spec start_link(binary(), inet:socket_address() | inet:hostname(), inet:port_number()) -> {ok, pid()} | {error, term()}.
 start_link(NetworkId, Address, Port) ->
     gen_server:start_link(?NETWORK_GID(NetworkId), ?MODULE, [NetworkId, Address, Port], []).
@@ -58,7 +65,7 @@ create_neural_network(_NetworkId, Layers, _) when length(Layers) < 3 ->
     {error, <<"Minimum number of layers is 3">>};
 create_neural_network(NetworkId, Layers, Weights) when is_list(Weights) ->
     create_neural_network(NetworkId, Layers, Weights, sigmoid);
-create_neural_network(NetworkId, [InputSize | Rest], Activation) when is_atom(Activation) ->
+create_neural_network(NetworkId, [InputSize | Rest], Activation) when is_atom(Activation) ->
     case lists:member(Activation, supported_activations()) of
         false -> {error, <<"Activation function not supported">>};
         true ->
@@ -82,58 +89,62 @@ supported_activations() -> [relu, sigmoid, softsign, tanh, tanh1].
 
 -spec initialize(binary(), pos_integer(), pos_integer()) -> ok | {error, binary()}.
 initialize(NetworkId, InpSize, OutSize) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {initialize, InpSize, OutSize}).
+    initialize(NetworkId, InpSize, OutSize, ?LEARNING_RATE).
+
+-spec initialize(binary(), pos_integer(), pos_integer(), float()) -> ok | {error, binary()}.
+initialize(NetworkId, InpSize, OutSize, LearningRate) ->
+    gen_server:call(?NETWORK_GID(NetworkId), {initialize, [InpSize, OutSize, LearningRate]}).
 
 -spec add_layer(binary(), pos_integer()) -> ok | {error, binary()}.
 add_layer(NetworkId, Size) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {add_layer, Size}).
+    gen_server:call(?NETWORK_GID(NetworkId), {add_layer, [Size]}).
 
--spec add_activation(binary(), atom()) -> ok | {error, binary()}.
+-spec add_activation(binary(), atom()) -> ok | {error, binary()}.
 add_activation(NetworkId, Activation) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {add_activation, Activation}).
+    gen_server:call(?NETWORK_GID(NetworkId), {add_activation, [Activation]}).
 
--spec set_cost(binary(), atom()) -> ok | {error, binary()}.
+-spec set_cost(binary(), atom()) -> ok | {error, binary()}.
 set_cost(NetworkId, CostFunc) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {set_cost, CostFunc}).
+    gen_server:call(?NETWORK_GID(NetworkId), {set_cost, [CostFunc]}).
 
--spec add_data_chunk(binary(), list(), list()) -> ok | {error, binary()}.
+-spec add_data_chunk(binary(), list(), list()) -> ok | {error, binary()}.
 add_data_chunk(NetworkId, Inputs, Labels) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {add_data_chunk, [Inputs, Labels]}).
+    add_data_chunk(NetworkId, Inputs, Labels, ?SCALE).
 
--spec add_data_chunk(binary(), list(), list(), list()) -> ok | {error, binary()}.
+-spec add_data_chunk(binary(), list(), list(), list()) -> ok | {error, binary()}.
 add_data_chunk(NetworkId, Inputs, Labels, Scale) ->
     gen_server:call(?NETWORK_GID(NetworkId), {add_data_chunk, [Inputs, Labels, Scale]}).
 
--spec set_learning_rate(binary(), float()) -> ok | {error, binary()}.
+-spec set_learning_rate(binary(), float()) -> ok | {error, binary()}.
 set_learning_rate(NetworkId, LearningRate) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {set_learning_rate, LearningRate}).
+    gen_server:call(?NETWORK_GID(NetworkId), {set_learning_rate, [LearningRate]}).
 
--spec train(binary()) -> ok | {error, binary()}.
-train(NetworkId) -> train(NetworkId, []).
+-spec train(binary()) -> ok | {error, binary()}.
+train(NetworkId) -> train(NetworkId, ?EPOCHS).
 
--spec train(binary(), pos_integer() | [pos_integer()]) -> ok | {error, binary()}.
-train(NetworkId, Epochs) when is_integer(Epochs) -> train(NetworkId, [Epochs]);
-train(NetworkId, Args) when is_list(Args) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {train, Args}).
+-spec train(binary(), pos_integer()) -> ok | {error, binary()}.
+train(NetworkId, Epochs) when is_integer(Epochs) ->
+    train(NetworkId, Epochs, ?BATCH_SIZE).
 
--spec train(binary(), pos_integer(), pos_integer()) -> ok | {error, binary()}.
-train(NetworkId, Epochs, BatchSize) -> train(NetworkId, [Epochs, BatchSize]).
+-spec train(binary(), pos_integer(), pos_integer()) -> ok | {error, binary()}.
+train(NetworkId, Epochs, BatchSize) ->
+    gen_server:call(?NETWORK_GID(NetworkId), {train, [Epochs, BatchSize]}).
 
--spec get_weights(binary()) -> ok | {error, binary()}.
+-spec get_weights(binary()) -> ok | {error, binary()}.
 get_weights(NetworkId) ->
-    gen_server:call(?NETWORK_GID(NetworkId), get_weights).
+    gen_server:call(?NETWORK_GID(NetworkId), {get_weights, []}).
 
 -spec set_weights(binary(), [float()]) -> ok | {error, binary()}.
 set_weights(NetworkId, Weights) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {set_weights, Weights}).
+    gen_server:call(?NETWORK_GID(NetworkId), {set_weights, [Weights]}).
 
--spec predict(binary(), [number()]) -> list() | {error, binary()} .
+-spec predict(binary(), [number()]) -> list() | {error, binary()} .
 predict(NetworkId, [N|_]=Input) when is_number(N) ->
     %% TODO: Check for errors...
-    [Result] = gen_server:call(?NETWORK_GID(NetworkId), {predict, [Input]}),
+    [Result] = predict(NetworkId, [Input]),
     Result;
 predict(NetworkId, Set) ->
-    gen_server:call(?NETWORK_GID(NetworkId), {predict, Set}).
+    gen_server:call(?NETWORK_GID(NetworkId), {predict, [Set]}).
 
 %% Gen server related functions
 
@@ -142,37 +153,9 @@ init([NetworkId, Address, Port]) ->
     {ok, Conn} = annlink_conn:connect(Address, Port),
     {ok, #state{networkId = NetworkId, conn = Conn}}.
 
-handle_call({initialize, InpSize, OutSize}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"initialize">>, [InpSize, OutSize]),
+handle_call({Operation, Args}, _From, #state{conn = Conn} = State) when is_atom(Operation) ->
+    Result = annlink_conn:call(Conn, atom_to_binary(Operation, utf8), Args),
     {reply, Result, State};
-handle_call({add_layer, Size}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"add_layer">>, [Size]),
-    {reply, Result, State};
-handle_call({add_activation, Activation}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"add_activation">>, [Activation]),
-    {reply, Result, State};
-handle_call({set_cost, CostFunc}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"set_cost">>, [CostFunc]),
-    {reply, Result, State};
-handle_call({add_data_chunk, Args}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"add_data_chunk">>, Args),
-    {reply, Result, State};
-handle_call({set_learning_rate, LearningRate}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"set_learning_rate">>, [LearningRate]),
-    {reply, Result, State};
-handle_call({train, Args}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"train">>, Args),
-    {reply, Result, State};
-handle_call(get_weights, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"get_weights">>, []),
-    {reply, Result, State};
-handle_call({set_weights, Weights}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"set_weights">>, [Weights]),
-    {reply, Result, State};
-handle_call({predict, Set}, _From, #state{conn = Conn} = State) ->
-    Result = annlink_conn:call(Conn, <<"predict">>, [Set]),
-    {reply, Result, State};
-
 handle_call(Req, _From, #state{networkId = NetworkId} = State) ->
     ?Error("Invalid call request ~p received by player ~p process ~p", [NetworkId, Req, self()]),
     {reply, {error, <<"invalid request">>}, State}.
